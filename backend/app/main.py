cae39477e6970fdd28f9a0a51d9e632a0f6ba0ca
RBxRobson <robsonnbarbosa0@gmail.com>
2025-04-11 19:17:08 -0300

    feat: Adiciona arquivo central da api

---
+++ backend/app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from database import engine
+from models import Base 
+from routes import auth
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5174"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+Base.metadata.create_all(bind=engine)
+
+app.include_router(auth.router)
